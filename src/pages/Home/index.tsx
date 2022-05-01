6b807bae9e27e7e44a083bd6e4be269f88902908
Eliveuton Souza <eliveuton3m@hotmail.com>
2022-05-01 13:41:12 -0300

    Made the transition to typescript

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 
-import { Card } from "../../components/Card";
+import { Card, CardProps } from "../../components/Card";
+
+type ProfileResponse = {
+  name: string;
+  avatar_url: string;
+};
+
+type User = {
+  name: string;
+  avatar: string;
+};
 
 export function Home() {
   const [studentName, setStudentName] = useState();
-  const [students, setStudents] = useState([]);
-  const [user, setUser] = useState({ name: "", avatar: "" });
+  const [students, setStudents] = useState<CardProps[]>([]);
+  const [user, setUser] = useState<User>({} as User);
 
   function handleAddStudent() {
     const newStudent = {
@@ -18,7 +28,7 @@ export function Home() {
       }),
     };
 
-    setStudents((prevState) => [...prevState, newStudent]);
+    setStudents((prevState: any) => [...prevState, newStudent]);
   }
 
   useEffect(() => {
@@ -26,7 +36,7 @@ export function Home() {
       const response = await fetch(
         "https://api.github.com/users/eliveutonsouza"
       );
-      const data = await response.json();
+      const data = (await response.json()) as ProfileResponse;
 
       setUser({
         name: data.name,
@@ -50,7 +60,7 @@ export function Home() {
       <input
         type="text"
         placeholder="Digite o nome..."
-        onChange={(e) => setStudentName(e.target.value)}
+        onChange={(e: any) => setStudentName(e.target.value)}
       />
 
       <button type="button" onClick={handleAddStudent}>
